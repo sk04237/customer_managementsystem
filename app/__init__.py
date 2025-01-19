fd8f08dcf5625369c116d0e7026181808e9c6482
sk04237 <g2342049@tcu.ac.jp>
2025-01-20 00:57:53 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# データベース管理用のSQLAlchemyインスタンスを作成
-
 db = SQLAlchemy()
 
 def create_app():
-    # Flaskアプリケーションのインスタンスを作成
     app = Flask(__name__)
 
-    # アプリの構成
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///customers.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = 'your_secret_key'
 
-    # データベース初期化
     db.init_app(app)
 
     with app.app_context():
-        from .app import main
+        from app.app import main  # 相対インポートから絶対パスに修正
         app.register_blueprint(main)
         db.create_all()
 
