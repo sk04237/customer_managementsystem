83526910dd758f9aabb20d2040c1185262d4371b
sk04237 <g2342049@tcu.ac.jp>
2025-01-31 15:21:59 +0900

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,12 @@
+/プロジェクトルート
+│── app/
+│   ├── __init__.py
+│   ├── app.py
+│   ├── models.py
+│   ├── templates/
+│   ├── static/
+│── db_backups/  # ← DB バックアップ保存用
+│── customers.db
+│── config.py    # ← ここに配置！
+│── requirements.txt
+│── run.py       # Flask アプリを起動するエントリーポイント
